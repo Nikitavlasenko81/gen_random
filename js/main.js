0978344b9ca7c7db4732b136c8483ca0a6caebfc
Nikitavlasenko81 <69199501+Nikitavlasenko81@users.noreply.github.com>
2021-06-15 10:53:36 +0300

    output of several random numbers

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+const displayRandomNumber = ()=>{
+    let form = document.forms[0];
+    const info = document.querySelector(`.info`)
+    const randomNumber = document.querySelector('.random-number')
+    const quantity = form.elements.quantity
 
-let form = document.forms[0];
-
-document.querySelector(".btn").addEventListener('click',()=>{
-    let min = +form.elements.firstNum.value;
-    let max = +form.elements.secondNum.value;
-    let res = getRndInteger(min, max);
-    document.querySelector('.random-number').innerHTML = res
-    document.querySelector(`.info`).innerHTML = `Сгенерировано случайгле число от ${min} до ${max} в ${new Date()}`
-})
+    document.querySelector(".btn").addEventListener('click',()=>{
+        randomNumber.innerHTML = ``
+        let min = +form.elements.firstNum.value;
+        let max = +form.elements.secondNum.value;
+        for(let i = 0; i < quantity.value; i++){
+            randomNumber.innerHTML += ` ${getRndInteger(min, max)}`
+        }
+        info.innerHTML = `Сгенерировано случайгле число(а) от ${min} до ${max} в ${new Date()}`
+    })
+}
+displayRandomNumber()
